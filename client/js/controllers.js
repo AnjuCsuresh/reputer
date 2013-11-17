9cf057ae1d8f5d3259f90be30243f11d7b91d2d3
manuchandraprasad <manuchandraprasadr@gmail.com>
2013-11-17 23:06:17 +0530

    3 tables added

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -291,7 +291,262 @@ angular.module('dashApp.controllers', []).
             })
         }
     })
+    //CONTROLLER - REVIEW
+    .controller('reviewTable', function ($http, $scope, User,$filter,$timeout,$routeParams,$cookieStore,$location) {
+        var id=$routeParams.id;
+        if(id>0){
+            $http.get(API_URL+'Entity/?id='+id+'&format=json').success(function (data) {
+                  $scope.entity=data.objects[0]
+                  if(data.objects[0].live==false){
+                    $location.path('/account/entity/oops');
+                  }
+            });
+        }
+        else{
+            var userid=$.cookie('the_cookie');
+            $http.get(API_URL+'Entity/?user__id='+userid+'&alive=true&live=true&format=json').success(function (data) {
+                    $scope.entities=data.objects
+                    if(data.objects.length>0){
+                        $scope.entity=data.objects[0]
+                    }
+                    else{
+                        $location.path('/account/entity');
+                    }
 
+                })
+        }
+        //PRODUCTION CODE: $http.get(DATA_API_URL+'getcrawltable/'+id).success(function(data, status, headers, config){
+        $http.get(DATA_API_URL+'getcrawltable/'+'11').success(function(data, status, headers, config){
+            $scope.items = data.aaData;
+            $scope.predicate = 'Rank';
+            $scope.reverse = false;
+            $scope.filteredItems = [];
+            $scope.groupedItems = [];
+            $scope.itemsPerPage = 3;
+            $scope.pagedItems = [];
+            $scope.currentPage = 0;
+            var searchMatch = function (haystack, needle) {
+            if (!needle) {
+                return true;
+            }
+            return String(haystack).toLowerCase().indexOf(needle.toLowerCase()) !== -1; 
+        };
+
+        // init the filtered items
+        $scope.search = function () {
+            $scope.filteredItems = $filter('filter')($scope.items, function (item) {
+                for(var attr in item) {
+                    if (item[attr]!=null){
+                       if (searchMatch(item[attr], $scope.query))
+                        return true; 
+                    }
+                    
+                }
+                return false;
+            });
+            // take care of the sorting order
+            if ($scope.sortingOrder !== '') {
+                $scope.filteredItems = $filter('orderBy')($scope.filteredItems, $scope.sortingOrder, $scope.reverse);
+            }
+            $scope.currentPage = 0;
+            // now group by pages
+            $scope.groupToPages();
+        };
+        
+        // calculate page in place
+        $scope.groupToPages = function () {
+            $scope.pagedItems = [];
+            
+            for (var i = 0; i < $scope.filteredItems.length; i++) {
+                if (i % $scope.itemsPerPage === 0) {
+                    $scope.pagedItems[Math.floor(i / $scope.itemsPerPage)] = [ $scope.filteredItems[i] ];
+                } else {
+                    $scope.pagedItems[Math.floor(i / $scope.itemsPerPage)].push($scope.filteredItems[i]);
+                }
+            }
+        };
+        
+        $scope.range = function (start, end) {
+            var ret = [];
+            if (!end) {
+                end = start;
+                start = 0;
+            }
+            for (var i = start; i < end; i++) {
+                ret.push(i);
+            }
+            return ret;
+        };
+        
+        $scope.prevPage = function () {
+            if ($scope.currentPage > 0) {
+                $scope.currentPage--;
+            }
+        };
+        
+        $scope.nextPage = function () {
+            if ($scope.currentPage < $scope.pagedItems.length - 1) {
+                $scope.currentPage++;
+            }
+        };
+        
+        $scope.setPage = function () {
+            $scope.currentPage = this.n;
+        };
+
+        // functions have been describe process the data for display
+        $scope.search();
+
+        // change sorting order
+        $scope.sort_by = function(newSortingOrder) {
+            if ($scope.sortingOrder == newSortingOrder)
+                $scope.reverse = !$scope.reverse;
+
+            $scope.sortingOrder = newSortingOrder;
+
+            // icon setup
+            $('th i').each(function(){
+                // icon reset
+                $(this).removeClass().addClass('icon-sort');
+            });
+            if ($scope.reverse)
+                $('th.'+newSortingOrder+' i').removeClass().addClass('icon-chevron-up');
+            else
+                $('th.'+newSortingOrder+' i').removeClass().addClass('icon-chevron-down');
+        };
+
+        
+        })
+        
+    })
+    //CONTROLLER - Changes
+    .controller('changesTable', function ($http, $scope, User,$filter,$timeout,$routeParams,$cookieStore,$location) {
+        var id=$routeParams.id;
+        if(id>0){
+            $http.get(API_URL+'Entity/?id='+id+'&format=json').success(function (data) {
+                  $scope.entity=data.objects[0]
+                  if(data.objects[0].live==false){
+                    $location.path('/account/entity/oops');
+                  }
+            });
+        }
+        else{
+            var userid=$.cookie('the_cookie');
+            $http.get(API_URL+'Entity/?user__id='+userid+'&alive=true&live=true&format=json').success(function (data) {
+                    $scope.entities=data.objects
+                    if(data.objects.length>0){
+                        $scope.entity=data.objects[0]
+                    }
+                    else{
+                        $location.path('/account/entity');
+                    }
+
+                })
+        }
+        //PRODUCTION CODE: $http.get(DATA_API_URL+'getchangestable/'+id).success(function(data, status, headers, config){
+        $http.get("/lib/changes.json").success(function(data, status, headers, config){
+            $scope.items = data.aaData;
+            $scope.predicate = 'Rank';
+            $scope.reverse = false;
+            $scope.filteredItems = [];
+            $scope.groupedItems = [];
+            $scope.itemsPerPage = 3;
+            $scope.pagedItems = [];
+            $scope.currentPage = 0;
+            var searchMatch = function (haystack, needle) {
+            if (!needle) {
+                return true;
+            }
+            return String(haystack).toLowerCase().indexOf(needle.toLowerCase()) !== -1; 
+        };
+
+        // init the filtered items
+        $scope.search = function () {
+            $scope.filteredItems = $filter('filter')($scope.items, function (item) {
+                for(var attr in item) {
+                    if (item[attr]!=null){
+                       if (searchMatch(item[attr], $scope.query))
+                        return true; 
+                    }
+                    
+                }
+                return false;
+            });
+            // take care of the sorting order
+            if ($scope.sortingOrder !== '') {
+                $scope.filteredItems = $filter('orderBy')($scope.filteredItems, $scope.sortingOrder, $scope.reverse);
+            }
+            $scope.currentPage = 0;
+            // now group by pages
+            $scope.groupToPages();
+        };
+        
+        // calculate page in place
+        $scope.groupToPages = function () {
+            $scope.pagedItems = [];
+            
+            for (var i = 0; i < $scope.filteredItems.length; i++) {
+                if (i % $scope.itemsPerPage === 0) {
+                    $scope.pagedItems[Math.floor(i / $scope.itemsPerPage)] = [ $scope.filteredItems[i] ];
+                } else {
+                    $scope.pagedItems[Math.floor(i / $scope.itemsPerPage)].push($scope.filteredItems[i]);
+                }
+            }
+        };
+        
+        $scope.range = function (start, end) {
+            var ret = [];
+            if (!end) {
+                end = start;
+                start = 0;
+            }
+            for (var i = start; i < end; i++) {
+                ret.push(i);
+            }
+            return ret;
+        };
+        
+        $scope.prevPage = function () {
+            if ($scope.currentPage > 0) {
+                $scope.currentPage--;
+            }
+        };
+        
+        $scope.nextPage = function () {
+            if ($scope.currentPage < $scope.pagedItems.length - 1) {
+                $scope.currentPage++;
+            }
+        };
+        
+        $scope.setPage = function () {
+            $scope.currentPage = this.n;
+        };
+
+        // functions have been describe process the data for display
+        $scope.search();
+
+        // change sorting order
+        $scope.sort_by = function(newSortingOrder) {
+            if ($scope.sortingOrder == newSortingOrder)
+                $scope.reverse = !$scope.reverse;
+
+            $scope.sortingOrder = newSortingOrder;
+
+            // icon setup
+            $('th i').each(function(){
+                // icon reset
+                $(this).removeClass().addClass('icon-sort');
+            });
+            if ($scope.reverse)
+                $('th.'+newSortingOrder+' i').removeClass().addClass('icon-chevron-up');
+            else
+                $('th.'+newSortingOrder+' i').removeClass().addClass('icon-chevron-down');
+        };
+
+        
+        })
+        
+    })
 //Edit entity cntrlr
     .controller('EntityEditCtrl', function ($http, $scope, User,$location,$timeout,$routeParams) {
         var id=$routeParams.id;
