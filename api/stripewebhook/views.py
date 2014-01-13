e3107e8c0c8ba5c1b50d849a7da690cbc4b3a428
manjushamohan <manjusha@quadloops.com>
2014-01-13 12:36:51 +0530

    Changed usd to $ in billinghistory

--- api/stripewebhook/views.py
+++ api/stripewebhook/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Create your views here.
 from django.contrib.auth.models import User
 from django.http import HttpResponse
@@ -10,6 +11,7 @@ import datetime
 def webhook(request):
     
     if request.method == 'POST':
+        currencies = {'USD': '$', 'AUD': '$', 'EUR': '€','BWP':'Pu','CAD':'$','GBP':'£','INR':'₹','NGN':'₦','ZAR':'R','ZWD':'Z$','JPY':'￥'}
         stripe.api_key ="sk_test_8a4R0xmqlVFFuQsfk3XjHpO5"  
         event_json = json.loads(request.body)
         print event_json
@@ -21,7 +23,7 @@ def webhook(request):
             if event_json['type']=="customer.card.created":
                 event.display_text="Added a new "+event_json['data']['object']['type']+" ending in "+event_json['data']['object']['last4']
             elif event_json['type']=="invoice.payment_succeeded":
-                event.display_text="Your invoice for "+event_json['data']['object']['currency']+" "+str(event_json['data']['object']['total'])+" was paid"
+                event.display_text="Your invoice for "+currencies[event_json['data']['object']['currency'].upper()]+str(float(event_json['data']['object']['total'])/100)+" was paid"
             elif event_json['type']=="customer.subscription.updated":
                 if "plan" in event_json['data']['previous_attributes']:
                     event.display_text="Your plan changed from "+event_json['data']['previous_attributes']['plan']['name']+" to "+event_json['data']['object']['plan']['name']
@@ -30,7 +32,7 @@ def webhook(request):
             elif event_json['type']=="customer.card.deleted":
                 event.display_text="Deleted a "+event_json['data']['object']['type']+" ending in "+event_json['data']['object']['last4']
             elif event_json['type']=="invoice.created":
-                event.display_text="You has a new invoice for "+event_json['data']['object']['currency']+" "+str(event_json['data']['object']['total'])
+                event.display_text="You has a new invoice for "+currencies[event_json['data']['object']['currency'].upper()]+str(float(event_json['data']['object']['total'])/100)
             elif event_json['type']=="customer.card.updated":
                 event.display_text="Updated a "+event_json['data']['object']['type']+" ending in "+event_json['data']['object']['last4']
             elif event_json['type']=="invoiceitem.updated":
@@ -40,15 +42,15 @@ def webhook(request):
             elif event_json['type']=="customer.subscription.trial_will_end":
                 event.display_text="Trial on the "+event_json['data']['object']['plan']['name']+" plan will end in 3 days"
             elif event_json['type']=="invoiceitem.created":
-                event.display_text="A proration adjustment for "+event_json['data']['object']['currency']+" "+str(event_json['data']['object']['amount'])+" was created for you"
+                event.display_text="A proration adjustment for "+currencies[event_json['data']['object']['currency'].upper()]+str(float(event_json['data']['object']['amount'])/100)+" was created for you"
             elif event_json['type']=="charge.succeeded":
-                event.display_text="You were charged "+event_json['data']['object']['currency']+" "+str(event_json['data']['object']['amount'])
+                event.display_text="You were charged "+currencies[event_json['data']['object']['currency'].upper()]+str(float(event_json['data']['object']['amount'])/100)
             elif event_json['type']=="customer.subscription.created":
                 event.display_text="Subscribed to the "+event_json['data']['object']['plan']['name']+" plan"
             elif event_json['type']=="customer.subscription.deleted":
                 event.display_text="Subscribed to the "+event_json['data']['object']['plan']['name']+" was cancelled"
             elif event_json['type']=="invoice.payment_failed":
-                event.display_text="Your invoice for "+event_json['data']['object']['currency']+" "+str(event_json['data']['object']['total'])+" failed payment"
+                event.display_text="Your invoice for "+currencies[event_json['data']['object']['currency'].upper()]+str(float(event_json['data']['object']['total'])/100)+" failed payment"
             elif event_json['type']=="charge.failed":
                 event.display_text="Your card has expired"
                 extendeduser = ExtendedUser.objects.get(stripe_customer=event_json['data']['object']['customer'])
@@ -81,10 +83,13 @@ def webhook(request):
             event.customer=event_json['data']['object']['id']
             event.type=event_json['type']
             if event_json['type']=="customer.updated":
-                if event_json['data']['previous_attributes']['default_card']==None:
-                    event.display_text="Your details were updated"
+                if "default_card" in event_json['data']['previous_attributes']:
+                    if event_json['data']['previous_attributes']['default_card']==None:
+                        event.display_text="Your details were updated"
+                    else:
+                        event.display_text="Your default credit card changed"
                 else:
-                    event.display_text="Your default credit card changed"
+                    event.display_text="Your details were updated"
             elif event_json['type']=="customer.created":
                 event.display_text="You are a new customer"
             event.event_data=event_json['data']
